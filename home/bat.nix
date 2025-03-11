31f96dc7cdd666cf10bc010d393e56fa132c4713
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-03-12 07:57:29 +1100

    chore: update theme

--- home/bat.nix
+++ home/bat.nix
@@ -11,18 +11,18 @@
   config = lib.mkIf config.home.bat.enable {
     programs.bat = {
       enable = true;
-      config.theme = "tokyonight";
-      themes = {
-        tokyonight = {
-          src = pkgs.fetchFromGitHub {
-            owner = "folke";
-            repo = "tokyonight.nvim";
-            rev = "057ef5d260c1931f1dffd0f052c685dcd14100a3";
-            sha256 = "sha256-1xZhQR1BhH2eqax0swlNtnPWIEUTxSOab6sQ3Fv9WQA=";
-          };
-          file = "extras/sublime/tokyonight_night.tmTheme";
-        };
-      };
+      config.theme = "Monokai Extended Bright";
+      # themes = {
+      #   tokyonight = {
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "folke";
+      #       repo = "tokyonight.nvim";
+      #       rev = "057ef5d260c1931f1dffd0f052c685dcd14100a3";
+      #       sha256 = "sha256-1xZhQR1BhH2eqax0swlNtnPWIEUTxSOab6sQ3Fv9WQA=";
+      #     };
+      #     file = "extras/sublime/tokyonight_night.tmTheme";
+      #   };
+      # };
     };
   };
 }
