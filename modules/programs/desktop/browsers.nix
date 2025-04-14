713dbb1d672c7fb2088c8ef2a45bb299dc20fb41
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-04-14 19:12:46 +1000

    feat: add zen

--- modules/programs/desktop/browsers.nix
+++ modules/programs/desktop/browsers.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   pkgs,
   lib,
   config,
@@ -13,6 +14,7 @@
       firefox
       chromium
       vivaldi
+      inputs.zen-browser.packages."${pkgs.system}".default
       # zen
     ];
   };
